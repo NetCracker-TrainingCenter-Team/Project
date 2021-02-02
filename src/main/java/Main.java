590de07f29072901d24f67fe74f1421d269dc28e
karma-cloud <75121027+karma-cloud@users.noreply.github.com>
2021-02-02 18:01:06 +0400

    Project
    Project

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       /* File file = new File("file.json");
+        Model.Category desert = new Model.Category("десерты");
+        Model.Dish dish1 = new Model.Dish("наполеон", desert, 150);
+        Model.Dish dish2 = new Model.Dish("мороженое",desert,100);
+        Model.Category drink = new Model.Category("напитки");
+        Model.Dish dish3 = new Model.Dish("сок",drink,200);
+        //Лист блюд
+        List<Model.Dish> dishes = List.of(dish1,dish2,dish3);
+        //Сериализация  десериализация
+        Controller.Serialize.serialize(dishes,file);
+        System.out.println(Controller.Serialize.deserialize(file));*/
+
+        //View.mainMenu();
+
+    }
+}
