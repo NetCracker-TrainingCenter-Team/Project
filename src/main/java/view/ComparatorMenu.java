6807d35b1fec8c72c1e417eb2871c1dbcfee7a34
mirai19 <61752311+mirai19@users.noreply.github.com>
2021-02-04 01:36:54 +0400

    Update ComparatorMenu.java

--- src/main/java/view/ComparatorMenu.java
+++ src/main/java/view/ComparatorMenu.java
@@ -9,13 +9,11 @@ public class ComparatorMenu implements Comparator<Object[]> {
     public int compare(Object[] o, Object[] t1) {
         int i=0;
 
-        String a = o[1].toString();
-        String b = t1[1].toString();
+        String a = o[1].toString().toUpperCase();
+        String b = t1[1].toString().toUpperCase();
 
-        while(i<a.length()&&i<b.length()&&(a.charAt(i)==b.charAt(i)))
-        {
+        while(i<a.length()&&i<b.length()&&(a.charAt(i)==b.charAt(i))) {
             i++;
-
         }
         if(i==a.length()&&i==b.length())
             return 0;
@@ -23,6 +21,10 @@ public class ComparatorMenu implements Comparator<Object[]> {
             return -1;
         else if(i>=b.length())
             return 1;
+
+
         return a.charAt(i)-b.charAt(i);
     }
 }
+
+
